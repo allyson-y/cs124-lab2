375e00476df06d4dcd13a84ff108d972e784b0bd
Yurie Muramatsu <yuriemuramatsu@Yuries-MacBook-Air.local>
2022-03-30 15:26:04 -0700

    change

--- src/SortButton.js
+++ src/SortButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+function showList(){
+
+}
+
+
 function SortButton(props) {
 
     return <div className="sortButton">
-        <button onClick="myFunction()" className="dropbtn">Dropdown</button>
+        <button onClick="showList()" className="dropbtn">Choose How to Sort</button>
         <div id="myDropdown" className="dropdown-content">
             <div className = "sortButton">
                  {props.todos.length > 1 && <button type={"button"} onClick={props.handleSortPriority} id = {"thisButton"}>
