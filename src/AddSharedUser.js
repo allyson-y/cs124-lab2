b326ce03d1486756df28c1ddafd1600841de0cc8
allyson-y <ayao20@holynames-sea.org>
2022-05-05 00:41:33 -0700

    sharing works

--- src/AddSharedUser.js
+++ src/AddSharedUser.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 function AddSharedUser(props) {
     return <>
-        {/*<form onSubmit={e => props.handleListChanged(props.id, "name", props.emails.concat([e.target.value]))}>*/}
-        {/*<form onSubmit={e => console.log(e.target.value)}>*/}
         <label>
                 <input
                     type="text"
@@ -14,16 +12,9 @@ function AddSharedUser(props) {
         <button
             value={props.value}
             onClick={(e) =>
-                    props.handleListChanged(props.id, "sharedWith", props.emails.contains(e.target.value) ? props.emails : props.emails.concat([e.target.value]))}>
+                    props.handleListChanged(props.id, "sharedWith", props.emails.concat([e.target.value]))}>
             Share
         </button>
-        {/*<input*/}
-        {/*    type="submit"*/}
-        {/*    value={props.value}*/}
-        {/*    onChange={(e) =>*/}
-        {/*        props.handleListChanged(props.id, "sharedWith", props.emails.concat([e.target.value]))}/>*/}
-            {/*<input type="submit" value="Share" />*/}
-        {/*</form>*/}
     </>
 }
 
