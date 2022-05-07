a17b19a6cbd780d9fd37ac18ae04cc09d203186e
Yurie Muramatsu <yuriemuramatsu@Yuries-MacBook-Air.local>
2022-05-06 21:32:39 -0700

    last

--- src/AddSharedUser.js
+++ src/AddSharedUser.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 function AddSharedUser(props) {
+
     return <>
         <label>
                 <input
                     type="text"
                     value={props.value}
                     onChange={props.handleChange}
-                    placeholder={"Email of user to share list with"}/>
+                    placeholder={"Email to share with"}/>
             </label>
         <button
             value={props.value}
             onClick={(e) =>
-                    props.handleListChanged(props.id, "sharedWith", props.emails.concat([e.target.value]))}>
+                    props.handleListChanged(props.id, "sharedWith", props.emails.concat([e.target.value])
+                        .catch(err => {console.log("error")}))
+        }>
             Share
         </button>
     </>
