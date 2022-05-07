d19269125630535d07614ff4d599f7b9f6f9357b
Yurie Muramatsu <yuriemuramatsu@Yuries-MacBook-Air.local>
2022-05-06 17:48:44 -0700

    final changes:

--- src/LogIn.js
+++ src/LogIn.js
@@ -19,74 +19,82 @@ function LogIn(props) {
         setEmailAlert(!emailAlert);
     }
 
-    function googleAlertToggle(){
+    function googleAlertToggle() {
         setGoogleAlert(!googleAlert);
     }
 
-    function signInWithEmail(email, password){
-        // if (emailError.code==='account-exists-with-different-credential'){
-        //     console.log("Wrong password")
+    function signInWithEmail(email, password) {
+
+        signInWithEmailAndPassword(email, password)
+        // {
+            setEmailAlert(!emailAlert);
+
+            // if (emailError) {
+            //     const errorCode = emailError.code;
+            //     const errorMessage = emailError.message;
+            //
+            //     if (errorCode === 'auth/wrong-password') {
+            //         alert('Wrong password');
+            //         // some code to fix
+            //     } else if (errorCode === 'auth/user-not-found') {
+            //         alert('User not found');
+            //     } else {
+            //         alert(errorMessage);
+            //     }
+            // }
         // }
-        // console.log(emailError.message);
-        // console.log(error);
-        if (emailError){
-            setEmailAlert(!emailAlert);
-        } else{
-            setEmailAlert(!emailAlert);
-            signInWithEmailAndPassword(email, password);
-        }
     }
 
-    if (googleUser || emailUser) {
-        // Shouldn't happen because App should see that
-        // we are signed in.
-        return <h1>Unexpectedly signed in already</h1>;
-    } else if (googleLoading || emailLoading) {
-        return <h1>Logging in…</h1>;
-    }
+if (googleUser || emailUser) {
+    // Shouldn't happen because App should see that
+    // we are signed in.
+    return <h1>Unexpectedly signed in already</h1>;
+} else if (googleLoading || emailLoading) {
+    return <h1>Logging in…</h1>;
+}
 
-    return (
+return (
+    <div>
+        {emailAlert && <Alert onClose={emailAlertToggle} onOK={emailAlertToggle}>
+            <p>Error logging in! {emailError ? emailError.message : "error"}</p></Alert>}
+
+        {googleAlert && <Alert onClose={googleAlertToggle} onOK={googleAlertToggle}>
+            <p>Error logging in! {googleError ? googleError.message : "error"}</p></Alert>}
+
+        <h1>Log In</h1>
         <div>
-            {emailAlert && <Alert onClose={emailAlertToggle} onOK={emailAlertToggle}>
-                <p>Error logging in! {emailError ? emailError.message : "error"}</p></Alert>}
-
-            {googleAlert && <Alert onClose={googleAlertToggle} onOK={googleAlertToggle}>
-                <p>Error logging in! {googleError ? googleError.message : "error"}</p></Alert>}
-
-            <h1>Log In</h1>
-            <div>
-                <label htmlFor="email">Email: </label>
-                <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br/>
-                <label htmlFor="password">Password: </label>
-                <input
-                    type="text"
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <br/>
-                <button onClick={() => signInWithEmail(email, password)}>
-                    Log In
-                </button>
-            </div>
+            <label htmlFor="email">Email: </label>
+            <input
+                type="text"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
             <br/>
-            <div>
-                <button onClick={() => signInWithGoogle()}>Sign in with Google</button>
-            </div>
-            Don't have an account?
-            <button onClick={props.toggleSignUp}>
-                Sign up
+            <label htmlFor="password">Password: </label>
+            <input
+                type="text"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br/>
+            <button onClick={() => signInWithEmail(email, password)}>
+                Log In
             </button>
         </div>
-    );
+        <br/>
+        <div>
+            <button onClick={() => signInWithGoogle()}>Sign in with Google</button>
+        </div>
+        Don't have an account?
+        <button onClick={props.toggleSignUp}>
+            Sign up
+        </button>
+    </div>
+);
 }
 
 export default LogIn;
